a695a7c55dc1933b62b075023f00a343ae40c407
Diiar <aitmarowd@gmail.com>
2019-11-16 13:01:24 +0600

    Этап 2. Добавил CRUD для товаров. + заглушку для фотографий.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy, reverse
+from django.views.generic import  ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView,DeleteView
+
+from webapp.models import Product
+
+class IndexView(ListView):
+    model = Product
+    template_name = 'index.html'
+
+    def get_queryset(self):
+        return Product.objects.all()
+
+
+class ProductView(DetailView):
+    model = Product
+    template_name = 'product/detail.html'
+
+
+class ProductCreateView(PermissionRequiredMixin, CreateView):
+    model = Product
+    template_name = 'product/create.html'
+    fields = ('name', 'category', 'description', 'photo')
+    permission_required = 'webapp.add_product'
+    permission_denied_message = '403 Доступ запрещён!'
+
+    def get_success_url(self):
+        return reverse('webapp:product_detail', kwargs={'pk': self.object.pk})
+
+
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
+    model = Product
+    template_name = 'product/update.html'
+    fields = ('name', 'category', 'description', 'photo')
+    context_object_name = 'product'
+
+    def get_success_url(self):
+        return reverse('webapp:product_detail', kwargs={'pk': self.object.pk})
+
+class ProductDeleteView(LoginRequiredMixin, DeleteView):
+    model = Product
+    template_name = 'product/delete.html'
+    success_url = reverse_lazy('webapp:index')
+    context_object_name = 'product'
